3f1dbff6d5740846b766f2086fcb9e538bebf37d
Olaf Bykowski <olafbykowski1@gmail.com>
2026-04-04 11:53:40 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] charsS = s.toCharArray();
+        char[] charsT = t.toCharArray();
+
+        Arrays.sort(charsS);
+        Arrays.sort(charsT);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charsS[i] != charsT[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
