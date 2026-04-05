4a502a711d1694622697dd2d3b4c293f00d24290
Olaf Bykowski <olafbykowski1@gmail.com>
2026-04-05 20:01:25 +0200

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> parenthesesMap = Map.of(
+            ')', '(', 
+            ']', '[', 
+            '}', '{'
+        );
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // char is a closed parentheses
+            if(parenthesesMap.containsKey(c)) {
+                if(stack.isEmpty()) return false;
+
+                if(stack.pop() == parenthesesMap.get(c)) {
+                    continue;
+                } else {
+                    return false;
+                }
+                
+            // char is a open parentheses
+            } else {                
+                stack.push(c);
+            }
+        }
+
+        if(!stack.isEmpty()) return false;
+
+        return true;
+    }
+}
